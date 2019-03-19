4b179f5264bb09d042bce053e87d3a5b51f29638
dmonsch <david.monschein@novatec-gmbh.de>
2019-03-19 20:24:30 +0100

    Small docker fix.

--- docker/start.sh
+++ docker/start.sh
@@ -9,7 +9,7 @@ cd /etc/pipeline/PCM-Calibration-Pipeline/modelrefinement.parameters.root/
 gradle bootJar
 
 # running
-cp /etc/pipeline/config.obj /etc/pipeline/PCM-Calibration-Pipeline/modelrefinement.parameters.root/modelrefinement.parameters.interface/build/libs/config.obj
+cp /etc/pipeline/config.json /etc/pipeline/PCM-Calibration-Pipeline/modelrefinement.parameters.root/modelrefinement.parameters.interface/build/libs/config.json
 cd /etc/pipeline/PCM-Calibration-Pipeline/modelrefinement.parameters.root/modelrefinement.parameters.interface/build/libs/
 
 java -jar modelrefinement-interface-0.1.0.jar
